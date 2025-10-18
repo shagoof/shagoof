62f29aee1081656e4dac51df15222b9807604375
Ahmed Ayman <ahamed.ayman220@gmail.com>
2025-10-18 16:04:39 +0300

    feat: Increase gap between brand logos from 0px to 30px
    - Updated featured-brands shortcode carousel spacing
    - Updated products page brand carousel spacing
    - Improves visual clarity and brand logo separation

--- platform/themes/martfury/partials/short-codes/featured-brands.blade.php
+++ platform/themes/martfury/partials/short-codes/featured-brands.blade.php
@@ -11,7 +11,7 @@
              data-owl-auto="true"
              data-owl-loop="false"
              data-owl-speed="10000"
-             data-owl-gap="0"
+             data-owl-gap="30"
              data-owl-nav="false"
              data-owl-dots="true"
              data-owl-item="7"

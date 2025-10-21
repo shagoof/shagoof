5fc6e022d9a0468e08c2050a6340f634cf5ed6bd
Ahmed Ayman <ahamed.ayman220@gmail.com>
2025-10-21 14:44:56 +0300

    Restructure product collections to match brands layout with images and clickable redirects

--- platform/themes/martfury/partials/short-codes/product-collections.blade.php
+++ platform/themes/martfury/partials/short-codes/product-collections.blade.php
@@ -1,44 +1,33 @@
-<div class="ps-product-list mb-60">
+<div class="mt-40 mb-40">
     <div class="ps-container">
-        <div class="ps-section__header">
-            <h3>{{ $title }}</h3>
-            <ul class="ps-section__links">
-                @foreach($productCollections->skip(1) as $item)
-                    <li class="nav-item">
-                        <a class="@if ($loop->first) active @endif" href="#" data-url="{{ route('public.ajax.products-by-collection', $item->id, ['limit' => $limit]) }}" title="{{ __('View products in') }} {{ $item->name }}">{{ $item->name }}</a>
-                    </li>
-                @endforeach
-            </ul>
-        </div>
-        <div class="ps-section__content">
-            <div class="half-circle-spinner loading-spinner d-none">
-                <div class="circle circle-1"></div>
-                <div class="circle circle-2"></div>
-            </div>
-            <div class="tab-pane fade show active" id="product-collections-product" role="tabpanel" aria-labelledby="product-collections-product-tab">
-                <div class="ps-carousel--nav owl-slider"
-                     data-owl-auto="false"
-                     data-owl-loop="false"
-                     data-owl-speed="10000"
-                     data-owl-gap="0"
-                     data-owl-nav="true"
-                     data-owl-dots="true"
-                     data-owl-item="7"
-                     data-owl-item-xs="2"
-                     data-owl-item-sm="2"
-                     data-owl-item-md="3"
-                     data-owl-item-lg="4"
-                     data-owl-item-xl="6"
-                     data-owl-duration="1000"
-                     data-owl-mousedrag="on"
-                >
-                    @foreach($products as $product)
-                        <div class="ps-product">
-                            {!! Theme::partial('product-item', compact('product')) !!}
-                        </div>
-                    @endforeach
+        @if($title)
+            <div class="ps-top-categories">
+                <div class="ps-section__header">
+                    <h3>{!! BaseHelper::clean($title) !!}</h3>
                 </div>
             </div>
+        @endif
+        <div class="ps-carousel--nav owl-slider"
+             data-owl-auto="true"
+             data-owl-loop="false"
+             data-owl-speed="10000"
+             data-owl-gap="30"
+             data-owl-nav="false"
+             data-owl-dots="true"
+             data-owl-item="7"
+             data-owl-item-xs="2"
+             data-owl-item-sm="2"
+             data-owl-item-md="3"
+             data-owl-item-lg="4"
+             data-owl-item-xl="6"
+             data-owl-duration="1000"
+             data-owl-mousedrag="on"
+        >
+            @foreach($productCollections->skip(1) as $collection)
+                <a href="{{ $collection->url }}" style="cursor: pointer;" title="{{ $collection->name }}">
+                    <img src="{{ RvMedia::getImageUrl($collection->image, null, false, RvMedia::getDefaultImage()) }}" alt="{{ $collection->name }}" loading="lazy"/>
+                </a>
+            @endforeach
         </div>
     </div>
 </div>

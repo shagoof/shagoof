20d3a9d80427731698f298ff720c0a95c323bca4
Ahmed Ayman <ahamed.ayman220@gmail.com>
2025-11-22 06:55:56 +0200

    Simplify bulk-price provider and remove settings panel integration

--- platform/plugins/bulk-price/src/Providers/BulkPriceServiceProvider.php
+++ platform/plugins/bulk-price/src/Providers/BulkPriceServiceProvider.php
@@ -7,9 +7,6 @@ namespace Botble\BulkPrice\Providers;
 use Botble\Base\Facades\DashboardMenu;
 use Botble\Base\Supports\ServiceProvider;
 use Botble\Base\Traits\LoadAndPublishDataTrait;
-use Botble\Setting\PanelSections\SettingOthersPanelSection;
-use Botble\Base\PanelSections\PanelSectionItem;
-use Botble\Base\PanelSections\PanelSectionManager;
 
 class BulkPriceServiceProvider extends ServiceProvider
 {
@@ -37,18 +34,6 @@ class BulkPriceServiceProvider extends ServiceProvider
                     'url' => route('bulk-price.index'),
                     'permissions' => ['bulk-price.index'],
                 ]);
-        });
-
-        PanelSectionManager::default()->beforeRendering(function (): void {
-            PanelSectionManager::registerItem(
-                SettingOthersPanelSection::class,
-                fn () => PanelSectionItem::make('bulk-price')
-                    ->setTitle(__('Bulk price'))
-                    ->withIcon('ti ti-currency-dollar')
-                    ->withPriority(430)
-                    ->withDescription(__('Bulk increase or decrease product prices by percentage or fixed amount.'))
-                    ->withRoute('bulk-price.index')
-            );
         });
     }
 }
